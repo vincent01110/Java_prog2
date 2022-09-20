760509a394854b55e96e620dc2ca15a836657d26
vincent01110 <bencekristof06@gmail.com>
2022-09-20 12:39:33 +0200

    idk

---
+++ Prog2/numero_dos/src/Inheritance/Main.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Teherauto teherauto = new Teherauto("AAA222", 144,
+                                            true, 2500);
+        System.out.println(teherauto.toString());
+    }
+}
