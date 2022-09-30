e3056ae755df8c56bfc775996a29aaa6a3c6527d
vincent01110 <bencekristof06@gmail.com>
2022-09-30 16:55:39 +0200

    O1G

--- Snake/src/MyNewWindow.java
+++ Snake/src/MyNewWindow.java
@@ -1,2 +1,36 @@
-public class MyNewWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyNewWindow extends JFrame implements ActionListener {
+
+    private JFrame frame = new JFrame("Snake");
+    private JButton startButton;
+    private JButton ladderBoardButton;
+    private JButton exitButton;
+    private JLabel background;
+    private JLabel label;
+
+    MyNewWindow(){
+
+        background = new JLabel();
+        background.setPreferredSize(new Dimension(500,500));
+        background.setBackground(Color.BLACK);
+        background.setOpaque(true);
+
+        startButton = new JButton("START");
+        startButton.setSize(200,75);
+
+        frame.add(background);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
